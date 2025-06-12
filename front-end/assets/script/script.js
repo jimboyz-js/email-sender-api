1151db4ac71a22f03b9528f73a58041386820236
Jimboyz <saronasda@gmail.com>
2025-06-12 18:52:53 +0800

    Update file

--- front-end/assets/script/script.js
+++ front-end/assets/script/script.js
@@ -1,5 +1,3 @@
-//fa-bounce
-
 const arrow = document.querySelector('.arrow-down');
 
 arrow.addEventListener('mouseenter', ()=> {
@@ -14,15 +12,48 @@ document.querySelectorAll('.btn-copy').forEach(button => {
     button.addEventListener('click', ()=> {
         const targetId = button.getAttribute('data-copy-target');
         const target = document.getElementById(targetId);
-        
-        if(target) {
+
+
+    
+        // if(target) {
+            // target.setAttribute('title', 'Copied!');
+            // target.title = 'Hello';
+
+            const icon = button.querySelector('i');
+
+            if (!target || !icon) return;
+
             navigator.clipboard.writeText(target.innerText)
             .then(()=> {
-                alert('Text Copied!');
+                // checkBtn.classList.add('showCheckIcon');
+                // copyBtn.classList.add('hideCopyIcon');
+                
+                
+
+                document.querySelectorAll('.btn-copy i').forEach(i => {
+                    i.className = 'fas fa-copy fa-lg';
+                })
+
+
+
+
+                icon.className = 'fas fa-check fa-lg';
+
+                setTimeout(() => {
+                    // copyBtn.classList.add('showCopyIcon');
+                    // copyBtn.classList.remove('hideCopyIcon');
+
+                    // checkBtn.classList.remove('showCheckIcon');
+                    // target.setAttribute('title', 'Copy');
+                    // target.title = 'Copy';
+
+                    icon.className = 'fas fa-copy fa-lg';
+                }, 5000);
+            
             })
             .catch(err => {
                 console.error('Failed to copy: ', err);
             })
-        }
+        // }
     })
 })
