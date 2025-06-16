5aca7fa5750cd46f987dc15fecfce2cdd926dcab
Jimboyz <saronasda@gmail.com>
2025-06-16 17:14:37 +0800

    Update File

--- front-end/assets/script/script.js
+++ front-end/assets/script/script.js
@@ -4,55 +4,41 @@ arrow.addEventListener('mouseenter', ()=> {
     arrow.classList.add('fa-bounce');
 })
 
-const text = document.getElementById('code');
-const copyBtn = document.querySelector('.fa-icon-copy');
-const checkBtn = document.querySelector('.fa-icon-check');
-
 document.querySelectorAll('.btn-copy').forEach(button => {
     button.addEventListener('click', ()=> {
         const targetId = button.getAttribute('data-copy-target');
         const target = document.getElementById(targetId);
+        const icon = button.querySelector('.btn-copy i');
 
+        if (!target || !icon) return;
 
-    
-        // if(target) {
-            // target.setAttribute('title', 'Copied!');
-            // target.title = 'Hello';
-
-            const icon = button.querySelector('.btn-copy i');
-
-            if (!target || !icon) return;
-
-            navigator.clipboard.writeText(target.innerText)
-            .then(()=> {
-                // checkBtn.classList.add('showCheckIcon');
-                // copyBtn.classList.add('hideCopyIcon');
-                
-
-                document.querySelectorAll('.btn-copy i').forEach(i => {
-                    i.className = 'fas fa-copy fa-lg';
-                })
-
-
-
-
-                icon.className = 'fas fa-check fa-lg';
-
-                setTimeout(() => {
-                    // copyBtn.classList.add('showCopyIcon');
-                    // copyBtn.classList.remove('hideCopyIcon');
-
-                    // checkBtn.classList.remove('showCheckIcon');
-                    // target.setAttribute('title', 'Copy');
-                    // target.title = 'Copy';
-
-                    icon.className = 'fas fa-copy fa-lg';
-                }, 3000);
-            
+        navigator.clipboard.writeText(target.innerText)
+        .then(()=> {
+            document.querySelectorAll('.btn-copy i').forEach(i => {
+                i.className = 'fas fa-copy fa-lg';
             })
-            .catch(err => {
-                console.error('Failed to copy: ', err);
-            })
-        // }
+
+            icon.className = 'fas fa-check fa-lg';
+            button.setAttribute('title', 'Copied!');
+
+            setTimeout(() => {
+                icon.className = 'fas fa-copy fa-lg';
+                button.title = 'Copy';
+            }, 3000);
+        })
+        .catch(err => {
+            console.error('Failed to copy: ', err);
+        })
+    })
+})
+
+document.querySelector('.btn-container button').addEventListener('click', ()=> {
+    const cdn = document.getElementById('code-1');
+    navigator.clipboard.writeText(cdn.innerText)
+    .then(()=> {
+        alert('CDN Copied!');
+    })
+    .catch(err => {
+        console.error('Failed to copy cdn: ', err);
     })
 })
