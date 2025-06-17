4d4e88eb95713a913fcc1d4cab2486eb258e7c22
Jimboyz <saronasda@gmail.com>
2025-06-17 16:02:32 +0800

    Update File

--- front-end/public/assets/script/readme.js
+++ front-end/public/assets/script/readme.js
@@ -8,9 +8,9 @@
 });
 
 Prism.highlightAll(); // Ensure Prism runs on dynamically added HTML
-fetch('../../README.md').then(r => r.text()).then(mdText => {
+fetch('https://raw.githubusercontent.com/jimboyz-js/email-sender-api/main/README.md')
+.then(r => r.text())
+.then(mdText => {
     document.getElementById('content').innerHTML = marked.parse(mdText);
     Prism.highlightAll();
 });
-  
-      
